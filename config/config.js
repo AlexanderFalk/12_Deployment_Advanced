aa0c44ab044cf4320bb36ea411453c49fd7b964a
alexfalk7@gmail.com <alexfalk7@gmail.com>
2016-12-06 10:07:01 +0100

    With Password

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //var url = 'mongodb://localhost:27017/store';
 // mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2
 module.exports = {
-    mongodb : 'mongodb://localhost:27017/store'
+    mongodb : 'mongodb://AlexanderFalk:A1234@ds127428.mlab.com:27428/deploymenttestdb'
 }
 
